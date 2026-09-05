d5be68801d2bff471fc0fc95d54d0d5474188bb0
david-bassler <davi.d.h.bassler@gmail.com>
2026-09-05 09:19:24 +0200

    Add offline service worker

---
+++ public/sw.js
@@ -0,0 +1,78 @@
+const CACHE_NAME = 'eds-diary-shell-v1'
+const APP_SHELL = [
+  './',
+  './manifest.webmanifest',
+  './icons/app-icon.svg',
+  './icons/app-icon-maskable.svg',
+]
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => cache.addAll(APP_SHELL))
+      .then(() => self.skipWaiting()),
+  )
+})
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((cacheNames) =>
+        Promise.all(
+          cacheNames
+            .filter(
+              (cacheName) =>
+                cacheName.startsWith('eds-diary-shell-') &&
+                cacheName !== CACHE_NAME,
+            )
+            .map((cacheName) => caches.delete(cacheName)),
+        ),
+      )
+      .then(() => self.clients.claim()),
+  )
+})
+
+self.addEventListener('fetch', (event) => {
+  const { request } = event
+
+  if (request.method !== 'GET') return
+
+  const requestUrl = new URL(request.url)
+  if (requestUrl.origin !== self.location.origin) return
+
+  if (request.mode === 'navigate') {
+    event.respondWith(
+      fetch(request).catch(async () => {
+        const cachedPage = await caches.match(request)
+        return cachedPage ?? caches.match('./')
+      }),
+    )
+    return
+  }
+
+  const cacheableDestinations = new Set([
+    'font',
+    'image',
+    'script',
+    'style',
+    'manifest',
+  ])
+
+  if (!cacheableDestinations.has(request.destination)) return
+
+  event.respondWith(
+    caches.match(request).then(async (cachedResponse) => {
+      if (cachedResponse) return cachedResponse
+
+      const networkResponse = await fetch(request)
+      if (!networkResponse.ok) return networkResponse
+
+      const cache = await caches.open(CACHE_NAME)
+      await cache.put(request, networkResponse.clone())
+
+      return networkResponse
+    }),
+  )
+})
