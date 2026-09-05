c4aa790c98e3305de0657fca8b128e54609ed589
david-bassler <davi.d.h.bassler@gmail.com>
2026-09-05 09:19:49 +0200

    Register service worker in production

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from './App'
+import { registerServiceWorker } from './registerServiceWorker'
 import './styles.css'
+
+registerServiceWorker()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
