8d36c2d5897b600e8fb276724b3107d5c69fbae4
david-bassler <davi.d.h.bassler@gmail.com>
2026-09-06 14:15:57 +0200

    Initialize local first data layer

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from './App'
+import { initializeDataLayer } from './data/initializeDataLayer'
 import { registerServiceWorker } from './registerServiceWorker'
 import './styles.css'
 
+initializeDataLayer()
 registerServiceWorker()
 
 createRoot(document.getElementById('root')!).render(
