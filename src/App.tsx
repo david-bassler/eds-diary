3684882bd10bbaef1a79d7090ed2da52af94f8aa
david-bassler <davi.d.h.bassler@gmail.com>
2026-09-06 14:15:58 +0200

    Expose Google sync settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,27 @@
-import { useState } from "react";
-import { TimeRangeColumn } from "./components/TimeRangeColumn/TimeRangeColumn";
-import type { TimeRange } from "./components/TimeRangeColumn/TimeRangeColumn";
+import { useState } from 'react'
+import { TimeRangeColumn } from './components/TimeRangeColumn/TimeRangeColumn'
+import type { TimeRange } from './components/TimeRangeColumn/TimeRangeColumn'
+import { GoogleSyncSettings } from './features/settings/GoogleSyncSettings'
 
 export function App() {
   const [range, setRange] = useState<TimeRange>({
-    start: "09:00",
-    end: "10:30",
-  });
+    start: '09:00',
+    end: '10:30',
+  })
 
   return (
     <main className="app">
-      <TimeRangeColumn
-        begin="06:00"
-        end="18:00"
-        resolution={15}
-        value={range}
-        onChange={setRange}
-        label="Zeitfenster planen"
-      />
+      <div className="app__content">
+        <TimeRangeColumn
+          begin="06:00"
+          end="18:00"
+          resolution={15}
+          value={range}
+          onChange={setRange}
+          label="Zeitfenster planen"
+        />
+        <GoogleSyncSettings />
+      </div>
     </main>
-  );
+  )
 }
