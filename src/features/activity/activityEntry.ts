8e3838ee365dc17d2fbae42b679ec593ade4e2b7
david-bassler <davi.d.h.bassler@gmail.com>
2026-09-07 21:03:24 +0200

    Add activityEntry.ts

---
+++ src/features/activity/activityEntry.ts
@@ -0,0 +1,21 @@
+export type ActivityEntryStatus = 'active' | 'deleted'
+
+export interface ActivityEntry {
+  id: string
+  date: string
+  startTime: string
+  endTime: string
+  activityName: string
+  note: string
+  status: ActivityEntryStatus
+  createdAt: string
+  updatedAt: string
+}
+
+export interface NewActivityEntry {
+  date: string
+  startTime: string
+  endTime: string
+  activityName: string
+  note?: string
+}
