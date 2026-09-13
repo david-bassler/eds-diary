b4635565b2f6aa138d849635d736fe52b4d8ddc3
david-bassler <davi.d.h.bassler@gmail.com>
2026-09-13 06:55:28 +0200

    Cache foot detail hitmap

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'eds-diary-shell-v29'
+const CACHE_NAME = 'eds-diary-shell-v30'
 const APP_SHELL = [
   './',
   './manifest.webmanifest',
@@ -21,6 +21,7 @@ const APP_SHELL = [
   './body-map/details/knee-front-right-hitmap.png?v=2',
   './body-map/details/knee-back-right-hitmap.png?v=1',
   './body-map/details/lower-back-hitmap.png?v=2',
+  './body-map/details/foot-dorsal-right-hitmap.png?v=1',
 ]
 
 self.addEventListener('install', (event) => {
