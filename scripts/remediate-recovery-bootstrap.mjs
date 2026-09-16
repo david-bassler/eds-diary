d54b360ab2ce7b495a4341d3f79513f6fce6fefa
david-bassler <davi.d.h.bassler@gmail.com>
2026-09-16 20:56:45 +0200

    Remove temporary security remediation tooling

--- scripts/remediate-recovery-bootstrap.mjs
+++
@@ -1,6 +0,0 @@
-import fs from 'node:fs'
-for(const path of ['src/data/recoveryProfile.ts','src/data/localDatabase.ts']){
-  let source=fs.readFileSync(path,'utf8')
-  source=source.replaceAll("revision.record_status!=='deleted')validateDomainData", "revision.record_status==='active')validateDomainData")
-  fs.writeFileSync(path,source)
-}
